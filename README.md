1f69586420e5e807bb55cb99c000720f27cd679f
Lim YingXuan <101786124+lunaroddity@users.noreply.github.com>
2025-10-17 20:32:56 +0800

    Clean up comments, update README, upload dataset and corpus

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # reddit-scraper
-Scraper for the relationship_advice subreddit
+This repository contains code for a scraper for the r/relationship_advice subreddit.
+
+## Information about the dataset and corpus
+An initial scrape was performed on the top 300 posts of all time in the r/relationship_advice subreddit. To mitigate the influence of other users in a user's narration of an interpersonal conflict that they experienced, updates that were posted as separate posts were excluded, resulting in a final dataset of 148 posts.
+
+The dataset `relationship_advice_data.csv` contains the following data:
+1. **title**, a string
+2. **body**, a string
+3. **gender**, a string in the set ('m', 'f')
+4. **age**, an integer
+5. **url**, the url linking to the original post
+
+In the r/relationship_advice subreddit, users self-report their age and gender, typically using the following format:
+> I/my/me ([age][M/F])
+
+Examples of declarations include: 'I (42M) broke up with...', 'my [27F] preference of...' or '...ignored me (35M)'
+
+A regular expression (regex) was thus used to capture the age and gender data. However, as users may not have followed the typical format in reporting their age and gender, the data may not have been captured by the regex. Thus, the dataset **needs to undergo further manual classification**.
+
+The corpus `relationship_advice_corpus.txt` contains the title and body of each post included in the dataset encoded in the UTF-8 format. 
--- scraper.py
+++ scraper.py
@@ -18,23 +18,23 @@ data = []
 relationship_advice = reddit.subreddit("relationship_advice")
 
 # scrape data from subreddit of top 300 posts of all time
-# for each post, scrape the text and the verdict (using the flair)
+# for each post, scrape the necessary data
 for submission in relationship_advice.top(limit=300):
-  # include only submissions with the verdict flairs, UPDATE posts excluded as they do not involve framing of moral judgement
+  # update posts excluded as they do not involve framing of moral judgement
   is_update = re.match(r'(?:.*)update(?:.*)', submission.title, re.IGNORECASE)
 
   if not is_update:
     title = submission.title
-    text = submission.selftext
+    body = submission.selftext
     url = submission.url
 
-  # from the text, identify the age and gender of the author using regex
+  # identify the age and gender of the author using regex
     age = None
     gender = None
     age_gender = None
 
-    # look for age and gender in the title following first-person singular pronouns 'i', 'my' or 'me'
-    # regex matches patterns like "I (25M)" or "my [f30]"
+    # author's self-reported age and gender typically follows the first-person singular pronouns 'i', 'my' or 'me'
+    # regex matches patterns like "I (25M)" or "my [f30]" or "me (m22)" etc.
     AGE_GENDER_RE = re.compile(
       r'(?:i|my|me)(?:\s*)(?:\(|\[)'
       r'(?P<gender1>[mf]*)(?:[^mf0-9])*(?P<age>\d{1,3})(?:[^mf0-9])*(?P<gender2>[mf]*)'
@@ -42,11 +42,12 @@ for submission in relationship_advice.top(limit=300):
       re.IGNORECASE
     )
 
-    # only search text if not found in title 
+    # only search body for author's age and gender if not found in title 
     age_gender = AGE_GENDER_RE.search(title)
     if age_gender is None:
-      age_gender = AGE_GENDER_RE.search(text)
+      age_gender = AGE_GENDER_RE.search(body)
  
+    # if age and/or gender reported in the typical format, extract them 
     if age_gender:
       age_str = age_gender.group('age')
       gender_str = age_gender.group('gender1') or age_gender.group('gender2')
@@ -57,20 +58,22 @@ for submission in relationship_advice.top(limit=300):
       if gender_str:
         gender = gender_str.lower()
 
-    data.append([title, text, gender, age, url])
+    # data still needs to be manually classified as some authors may not report their age or gender in the typical format
+    data.append([title, body, gender, age, url])
 
 # create a dataframe from the data
-df = pd.DataFrame(data, columns=["title", "text", "gender", "age", "url"])
+df = pd.DataFrame(data, columns=["title", "body", "gender", "age", "url"])
 
-# convert the dataframe into a sql table to allow for custom queries to generate various corpuses later
-# engine = create_engine("sqlite://", echo=True)
-# df.to_sql("relationship_advice_posts", engine, if_exists="replace", index=False)
-
-# output separate files for each type of post
-# csv with all the data
+# output the following files:
+# csv with all data collected
 df.to_csv("relationship_advice_data.csv", index=False)
 
-# txt file with utf-8 encoding for corpus creation
+# txt file encoded in the utf-8 format for the creation of a corpus
 with open("relationship_advice_corpus.txt", "w", encoding="utf-8") as f:
-  full_text = df[['title', 'text']].apply(lambda x: '\n'.join(x), axis=1).str.cat(sep='\n\n\n\n')
+  full_text = df[['title', 'body']].apply(lambda x: '\n'.join(x), axis=1).str.cat(sep='\n\n\n\n')
   f.write(full_text)
+
+# possible extension: convert the dataframe into a sql table to allow for custom queries
+# about author's age or gender to generate various filtered corpuses later, need to exclude null values first 
+# engine = create_engine("sqlite://", echo=True)
+# df.to_sql("relationship_advice_posts", engine, if_exists="replace", index=False)
