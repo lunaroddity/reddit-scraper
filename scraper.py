064a439a61ced85ef33cac42f2eaecf5eca93126
Lim YingXuan <101786124+lunaroddity@users.noreply.github.com>
2025-10-17 17:11:07 +0800

    Add age and gender identification functionality, change source subreddit

--- scraper.py
+++ scraper.py
@@ -1,7 +1,9 @@
 from dotenv import load_dotenv
 import os
+import re
 import praw
 import pandas as pd
+# from sqlachemy import create_engine
 
 load_dotenv()
 
@@ -12,40 +14,63 @@ reddit = praw.Reddit(
   user_agent=os.getenv("REDDIT_USER_AGENT")
 )
 
-aita = reddit.subreddit("AmItheAsshole")
-
-# scrape data from subreddit of top 200 posts of all time
-# for each post, scrape the text and the verdict (using the flair)
 data = []
-valid_flairs = ["Asshole", "Not the A-hole", "Everyone Sucks", "No A-holes Here"]
+relationship_advice = reddit.subreddit("relationship_advice")
 
-for submission in aita.top(limit=10):
-  # include only submissions with the verdict flairs, UPDATE posts excluded
-  # so as to maximise the number of unique authors
-  # TODO: may not be 200 posts exactly since META and UPDATE posts excluded
-  if submission.link_flair_text in valid_flairs:
-    id = submission.id
+# scrape data from subreddit of top 300 posts of all time
+# for each post, scrape the text and the verdict (using the flair)
+for submission in relationship_advice.top(limit=300):
+  # include only submissions with the verdict flairs, UPDATE posts excluded as they do not involve framing of moral judgement
+  is_update = re.match(r'(?:.*)update(?:.*)', submission.title, re.IGNORECASE)
+
+  if not is_update:
     title = submission.title
     text = submission.selftext
-    verdict = submission.link_flair_text
     url = submission.url
 
   # from the text, identify the age and gender of the author using regex
+    age = None
+    gender = None
+    age_gender = None
 
-  # if not found, set to None (to be determined manually later)
+    # look for age and gender in the title following first-person singular pronouns 'i', 'my' or 'me'
+    # regex matches patterns like "I (25M)" or "my [f30]"
+    AGE_GENDER_RE = re.compile(
+      r'(?:i|my|me)(?:\s*)(?:\(|\[)'
+      r'(?P<gender1>[mf]*)(?:[^mf0-9])*(?P<age>\d{1,3})(?:[^mf0-9])*(?P<gender2>[mf]*)'
+      r'(?:(\)|\]))',
+      re.IGNORECASE
+    )
 
-  # data.append([id, title, text, gender, age, verdict, url])
-    data.append([id, title, text, verdict, url])
-  # print(data)
+    # only search text if not found in title 
+    age_gender = AGE_GENDER_RE.search(title)
+    if age_gender is None:
+      age_gender = AGE_GENDER_RE.search(text)
+ 
+    if age_gender:
+      age_str = age_gender.group('age')
+      gender_str = age_gender.group('gender1') or age_gender.group('gender2')
+
+      if age_str:
+        age = int(age_str)
+      
+      if gender_str:
+        gender = gender_str.lower()
+
+    data.append([title, text, gender, age, url])
 
 # create a dataframe from the data
-df = pd.DataFrame(data, columns=["id", "title", "text", "verdict", "url"])
+df = pd.DataFrame(data, columns=["title", "text", "gender", "age", "url"])
+
+# convert the dataframe into a sql table to allow for custom queries to generate various corpuses later
+# engine = create_engine("sqlite://", echo=True)
+# df.to_sql("relationship_advice_posts", engine, if_exists="replace", index=False)
 
 # output separate files for each type of post
 # csv with all the data
-# df.to_csv("aita_data.csv", index=False)
+df.to_csv("relationship_advice_data.csv", index=False)
 
 # txt file with utf-8 encoding for corpus creation
-with open("aita_corpus.txt", "w", encoding="utf-8") as f:
-  full_text = df.to_string(columns=["text"], justify="left", line_width=80, header=False, index=False)
+with open("relationship_advice_corpus.txt", "w", encoding="utf-8") as f:
+  full_text = df[['title', 'text']].apply(lambda x: '\n'.join(x), axis=1).str.cat(sep='\n\n\n\n')
   f.write(full_text)
